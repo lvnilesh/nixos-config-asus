e9feda72d935f00aef0db0e721c5ac9a605209af
Nilesh <nilesh@cloudgeni.us>
2025-05-01 00:54:25 +0000

    86 current  2025-05-01 00:46:34  25.05.20250412.2631b0b   6.12.23                          *

--- modules/virt.nix
+++ modules/virt.nix
@@ -8,11 +8,25 @@
     spiceUSBRedirection.enable = true;
   };
 
-  networking.interfaces.eno1.useDHCP = true;
-  networking.interfaces.br0.useDHCP = true;
-  networking.bridges = {
-    "br0" = {
-      interfaces = ["eno1"];
-    };
+  networking.networkmanager.unmanaged = ["interface-name:eno1" "interface-name:br0"];
+
+  networking.useDHCP = false; # Optional: Disable top-level DHCP if you configure all interfaces explicitly
+  networking.interfaces.eno1 = {
+    # Do NOT configure useDHCP or IP addresses here
+    # This interface will be managed by the bridge
+    # You might need specific L2 settings here eventually, but usually not.
+  };
+
+  networking.interfaces.br0 = {
+    useDHCP = true; # Get IP address for the bridge interface
+  };
+
+  networking.bridges."br0" = {
+    interfaces = ["eno1"]; # Add eno1 to the bridge
+    # Optional: You might want to set bridge priority, STP, etc. here if needed
+    # settings = {
+    #   ForwardDelay = 4;
+    #   StpEnable = true;
+    # };
   };
 }
