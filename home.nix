0449b7ac0ec4d17ffe01ce87e957d75f3bcf0521
Nilesh <nilesh@cloudgeni.us>
2025-05-01 05:48:47 +0000

    94 current  2025-05-01 05:48:42  25.05.20250412.2631b0b   6.12.23                          *

--- home.nix
+++ home.nix
@@ -15,6 +15,12 @@
   fonts.fontconfig.enable = true;
   xdg.enable = true;
 
+  # set cursor size and dpi for 27 inch 5k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 218; # 172 for 4k;
+  };
+
   dconf.settings = {
     "org/gnome/shell" = {
       enabled-extensions = [
@@ -240,10 +246,92 @@
       ])
       azure-cli
       awscli2
+      # notify
+
       libgtop
       # Add the .dev output for header files needed for compilation
       libgtop.dev
+      dunst # notify-send
+      mako # notify-send
+      libnotify # notify-send
+      glib # notify-send
+
+      # Packages that should be installed to the user profile.
+      neofetch
+      nnn # terminal file manager
+
+      # archives
+      zip
+      xz
+      unzip
+      p7zip
+
+      # utils
+      ripgrep # recursively searches directories for a regex pattern
+      jq # A lightweight and flexible command-line JSON processor
+      yq-go # yaml processor https://github.com/mikefarah/yq
+      eza # A modern replacement for ‘ls’
+      fzf # A command-line fuzzy finder
+
+      # networking tools
+      nmap
+      mtr # A network diagnostic tool
+      iperf3
+      dnsutils # `dig` + `nslookup`
+      ldns # replacement of `dig`, it provide the command `drill`
+      aria2 # A lightweight multi-protocol & multi-source command-line download utility
+      socat # replacement of openbsd-netcat
+      nmap # A utility for network discovery and security auditing
+      ipcalc # it is a calculator for the IPv4/v6 addresses
+
+      # misc
+      cowsay
+      file
+      which
+      tree
+      gnused
+      gnutar
+      gawk
+      zstd
+      gnupg
+
+      # nix related
+      #
+      # it provides the command `nom` works just like `nix`
+      # with more details log output
+      nix-output-monitor
+
+      # productivity
+      glow # markdown previewer in terminal
+
+      btop # replacement of htop/nmon
+      iotop # io monitoring
+      iftop # network monitoring
+
+      # system call monitoring
+      strace # system call monitoring
+      ltrace # library call monitoring
+      lsof # list open files
+
+      # system tools
+      sysstat
+      lm_sensors # for `sensors` command
+      ethtool
+      pciutils # lspci
+      usbutils # lsusb
     ]);
+
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
 
   systemd.user.services.albert = {
     Unit = {
@@ -280,12 +368,15 @@
     alacritty = {
       enable = true;
       settings = {
+        env.TERM = "xterm-256color";
         window.opacity = 1.0;
         font.normal = {
           family = "JetBrains Mono";
           style = "Regular";
         };
-        font.size = 16;
+        font.size = 12;
+        scrolling.multiplier = 5;
+        selection.save_to_clipboard = true;
       };
     };
     git = {
@@ -338,7 +429,7 @@
       enableCompletion = true;
       envExtra = ''
         export ZOMEZSHVARIABLE="something";
-        alias ls=eza;
+        alias ls="eza -al";
         alias k=kubectl;
       '';
     };
