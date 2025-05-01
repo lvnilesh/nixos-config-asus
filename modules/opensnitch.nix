c61367602b1c075d6187c6c46400c3899e309750
Nilesh <nilesh@cloudgeni.us>
2025-05-01 00:30:36 +0000

    85 current  2025-05-01 00:30:30  25.05.20250412.2631b0b   6.12.23                          *

--- modules/opensnitch.nix
+++ modules/opensnitch.nix
@@ -13,7 +13,7 @@
 
   # A list of general rules needed no matter how the system is configured
   services.opensnitch = {
-    enable = false;
+    enable = true;
     settings.DefaultAction = "deny";
     rules = {
       rule-000-localhost = {
